1ee77fd7f47643453f7e4b226f2524bb5fe3aad8
mcmgeomagic1 <59767266+mcmgeomagic1@users.noreply.github.com>
2020-12-08 11:17:06 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,25 +1,17 @@
 	let botao = document.querySelector('.btn')
 	let glass = document.querySelector('.glass')
     let fechaContexto = document.querySelector('.fecha')
-    let caracaoDiv = document.querySelector('.coracao-div')
     let coracao = document.querySelector('.coracao')
-    let coracaoActivo = document.querySelector('.coracao-activo')
 
 	botao.addEventListener("click", ()=>{
 		glass.style.display = "block";
-		glass.style.animation = "zoomOne .2s ease-in-out"
 		botao.style.display = "none"
 	})
 
 	fechaContexto.addEventListener("click", ()=>{
-		glass.style.animation = "zoomOne2 .4s ease-in-out"
-		setTimeout(esperaEFecha, 200)
-	})
-
-	const esperaEFecha =()=> {
 		glass.style.display = "none"
 		botao.style.display = "block"
-	}
+	})
 
 	coracao.addEventListener("click", ()=> {
 		
